87d6c429e1221e33c1603f421f9cbb9feed0809f
QAmamomamo <150163062+qa-marmot@users.noreply.github.com>
2026-03-12 15:56:01 +0900

    Update QuestionView.tsx
    型追加

--- js-exam-next/src/components/QuestionView.tsx
+++ js-exam-next/src/components/QuestionView.tsx
@@ -11,6 +11,8 @@ import { useRef } from "react";
 type StudyLevel = "beginner" | "intermediate" | "advanced";
 
 interface Question {
+  id: number;
+  category: string;
   question: string;
   options: string[];
   correct: number;
